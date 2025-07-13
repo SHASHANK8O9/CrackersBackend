75a973d8c7e6f65f5f3ffa4e5552690d404cc9eb
Negi Nivex <71958480+SHASHANK8O9@users.noreply.github.com>
2025-07-13 17:55:28 +0530

    Update order.js

--- src/models/order.js
+++ src/models/order.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema(
   {
-    fullName: { type: String, required: true, unique: true },
+    fullName: { type: String, required: true },
     email: { type: String, required: true },
     phone: { type: String, required: true },
     address: { type: String, required: true },
